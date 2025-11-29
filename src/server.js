e3ad7f98f83eaee3966519886ded6c4576c16ab5
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2025-11-29 12:44:14 +0200

    all

--- src/server.js
+++ src/server.js
@@ -2,10 +2,10 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import { connectMongoDB } from './db/connectMongoDB.js';
-import { logger } from './middleware/logger';
-import { notFoundHandler } from './middleware/notFoundHandler';
-import { errorHandler } from './middleware/errorHandler';
-import notesRoutes from './routes/notesRoutes';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
